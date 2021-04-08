3c4990480b863dd0cea7cd9f212b4032ef3d3abb
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-08 16:07:23 -0300

    Cria teste para criação de Contatos.

--- tests/Feature/Contacts/ContactCreateTest.php
+++ tests/Feature/Contacts/ContactCreateTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\Contacts;
 
+use App\Http\Livewire\Contacts\ContactNew;
 use App\Models\Contact;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
 use Tests\TestCase;
 
 /**
@@ -23,5 +25,12 @@ class ContactCreateTest extends TestCase
         $this->actingAs($user = User::factory()->withPersonalTeam()->create());
 
         $contactFake = Contact::factory()->make();
+
+        Livewire::test(ContactNew::class)
+            ->call('mount', $contactFake)
+            ->call('store')
+            ->assertEmitted('created');
+
+        $this->assertDatabaseHas('contacts', $contactFake->toArray());
     }
 }
